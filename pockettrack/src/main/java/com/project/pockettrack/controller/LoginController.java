573900c62e62cf5e11f7c74599f7a074aafa1788
Lenny <chinluke.lin@gmail.com>
2024-10-06 17:02:04 -0700

    version 0.01

---
+++ pockettrack/src/main/java/com/project/pockettrack/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.project.pockettrack.controller;
+/**
+ * <p>
+ *  LoginController 接口
+ * </p>
+ *
+ * @author Cuimei Fang
+ * @since 2024-10-06
+ */
+public class LoginController {
+
+}
---
+++ pockettrack/src/main/java/com/project/pockettrack/request/UserLoginRequest.java
@@ -0,0 +1,5 @@
+package com.project.pockettrack.request;
+
+public class UserLoginRequest {
+
+}
